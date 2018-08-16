aa73436f50380f0ee5f15638d7723300021c375d
Sannidhi Teredesai <sannidhi.s.t@gmail.com>
2018-08-16 10:45:54 +0530

    Added tests and code refactored

--- pa/fd.py
+++ pa/fd.py
@@ -1,17 +1,24 @@
-fds = []
+from copy import deepcopy as copy_of
+
+class FDCollection:
+    
+    def __init__(self):
+        self.fds = []
+
+    def add(self, new_fd):
+        if new_fd in self.fds:
+            raise __class__.AlreadyExistsException
+        self.fds.append(new_fd)
 
 
-def add(new_fd):
-    global fds
-    if new_fd in fds:
-        raise AlreadyExistsException
-    fds.append(new_fd)
+    def exists(self, new_fd):
+        return new_fd in self.fds
 
 
-def exists(new_fd):
-    return new_fd in fds
+    def get_all_fds(self):
+        return copy_of(self.fds)
 
 
-class AlreadyExistsException(Exception):
-    pass
+    class AlreadyExistsException(Exception):
+        pass
 
--- tests/test_fd.py
+++ tests/test_fd.py
@@ -1,9 +1,10 @@
 import pytest
-import pa.fd as fd
+from pa.fd import FDCollection
 
 
-def test_new_fd_is_added():
-    new_fd = {
+@pytest.fixture
+def new_fd():
+    return {
         'bank': 'bank1',
         'branch': 'branch1',
         'first_name': 'customer1',
@@ -19,27 +20,23 @@ def test_new_fd_is_added():
         'maturity_amt': 0,
         'nomination': '',
     }
-    fd.add(new_fd)
-    assert fd.exists(new_fd) == True, "New FD was not added"
+
+
+def test_new_fd_is_added():
+    fd_collection = FDCollection()
+    fd_collection.add(new_fd)
+    assert fd_collection.exists(new_fd) == True, "New FD was not added"
 
 
 def test_add_existing_fd_raises_exception():
-    with pytest.raises(fd.AlreadyExistsException):
-        new_fd = {
-            'bank': 'bank1',
-            'branch': 'branch1',
-            'first_name': 'customer1',
-            'joint_name': 'customer2',
-            'mode': 'single',
-            'number': 'FD1',
-            'auto_renewal': True,
-            'start_date': '',
-            'end_date': '',
-            'period': '',
-            'roi_pcpa': 0,
-            'principal_amt': 0,
-            'maturity_amt': 0,
-            'nomination': '',
-        }
-        fd.add(new_fd)
-        fd.add(new_fd)
+    with pytest.raises(FDCollection.AlreadyExistsException):
+        fd_collection = FDCollection()
+        fd_collection.add(new_fd)
+        fd_collection.add(new_fd)
+
+
+def test_get_all_fds():
+    fd_collection = FDCollection()
+    assert fd_collection.get_all_fds() == []
+    fd_collection.add(new_fd)
+    assert fd_collection.get_all_fds() == [new_fd]
