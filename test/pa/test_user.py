2890640c8fcab0029dad6be0aa403463e157331a
sannidhiteredesai <sannidhiteredesai@users.noreply.github.com>
2019-03-22 21:16:22 +0530

    Adding tests to check transformation in User class

--- test/pa/test_user.py
+++ test/pa/test_user.py
@@ -1,11 +1,17 @@
 from unittest import TestCase
-from pa.pa.user import Users
+from pa.pa.user import User
+from datetime import datetime
 
 class TestUser(TestCase):
 
-    def setUp(self):
-        self.users = Users()
-
-    # def test_user_exists(self):
-    #     self.users.add()
-    #     self.assertTrue(self.users.exists('user1'))
+    def test_user_exists(self):
+        user = User({
+            'username': 'user1',
+            'password': 'password',
+            'mobile': '1234567890',
+            'dob': datetime(1991, 1, 2),
+            'secret_question': 'Some question ?',
+            'answer': 'Some nswer',
+        })
+        self.assertTrue(1234567890, user.mobile)
+        self.assertTrue('02/01/1991', user.dob)
