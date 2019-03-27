3eadc878d6918a56f2822185f657c7689d386d50
sannidhiteredesai <sannidhiteredesai@users.noreply.github.com>
2019-03-27 07:16:02 +0530

    Added test for get all members of a user

--- pa/pa/member.py
+++ pa/pa/member.py
@@ -11,3 +11,10 @@ class Members:
     def exists(self, member, for_user):
         return self.members.exists(member, for_user)
 
+    def get_members_of_user(self, user):
+        """Returns sorted list of members"""
+        members = []
+        for member in self.members.get_members(for_user=user):
+            if 'membername' in member:
+                members.append(member['membername'])
+        return sorted(members)
--- test/pa/test_member.py
+++ test/pa/test_member.py
@@ -38,3 +38,19 @@ class TestMember(TestCase):
         members = Members(config=TestConfig)
         members.add(member='m1', for_user='u1')
         mock_add.assert_called_with('m1', 'u1')
+
+    @patch('pa.db.member.MemberDB.get_members')
+    def test_get_members_of_user(self, mock_get_members):
+        members = Members(config=TestConfig)
+
+        mock_get_members.return_value = [{'membername': 'm1', 'username': 'u1'},
+                                         {'membername': 'm3', 'username': 'u1'},
+                                         {'membername': 'm2', 'username': 'u1'}]
+        self.assertEqual(['m1', 'm2', 'm3'], members.get_members_of_user('u1'))
+
+        mock_get_members.return_value = [{'membername': 'm5', 'username': 'u2'},
+                                         {'membername': 'm4', 'username': 'u2'}]
+        self.assertEqual(['m4', 'm5'], members.get_members_of_user('u2'))
+
+        mock_get_members.return_value = []
+        self.assertEqual([], members.get_members_of_user('u3'))
