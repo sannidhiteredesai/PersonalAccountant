31576ae53185a93c3d9c9a20a9de95e93c6a5cb4
sannidhiteredesai <sannidhiteredesai@users.noreply.github.com>
2019-04-08 01:01:06 +0530

    Refactoring and passing all bank-branches as input to UI

--- pa/ui/fd/routes.py
+++ pa/ui/fd/routes.py
@@ -2,12 +2,22 @@ from flask import render_template
 from flask_login import login_required, current_user
 from pa.ui import app
 from pa.ui.fd.forms import FDForm
+from pa.pa.bank import Banks
+from pa.pa.member import Members
 
 
 @login_required
 @app.route('/fd', methods=['GET', 'POST'])
 def fd():
-    form = FDForm(current_user)
+    banks = Banks()
+    members = Members()
+    bank_branches = banks.get_all_bank_branch_names(for_user=current_user.username)
+    form = FDForm(banks.get_all_bank_branch_names(for_user=current_user.username),
+                  members.get_members_of_user(user=current_user.username))
+
+    if form.validate_on_submit():
+        return f'Trying to save: {form.data}'
+
     fds = [{
         'fd_bank': 'This is bank name',
         'bank_branch': 'This is Bank Branch Name',
@@ -23,6 +33,6 @@ def fd():
         'roi': '8.25',
         'princpal_amount': '100000',
         'maturity_amount': '100000',
-    } for i in range(1, 11)]
+    } for i in range(1, 5)]
     return render_template('fd/fd.html', title='Fixed Deposits', form=form,
-                           fds=fds)
+                           fds=fds, branches=bank_branches)
