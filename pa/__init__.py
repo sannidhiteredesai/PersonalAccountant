c6354664d2d807b577ef662c4587271d0736c48c
sannidhiteredesai <sannidhiteredesai@users.noreply.github.com>
2019-04-08 19:02:03 +0530

    Changing db file to be stored in pretty format

--- pa/__init__.py
+++ pa/__init__.py
@@ -2,5 +2,8 @@ from tinydb import TinyDB
 from tinydb.storages import MemoryStorage
 from pa.config import Config
 
+
 def get_db(config=Config):
-    return TinyDB(storage=MemoryStorage) if config.DB == 'memory' else TinyDB(config.DB)
+    return TinyDB(storage=MemoryStorage) if config.DB == 'memory' else TinyDB(config.DB,
+                                                                              indent=4,
+                                                                              separators=(',', ': '))
