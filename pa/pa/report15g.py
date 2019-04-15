f54e570f1461ab649dccec9ec6cd92ba6ee7c907
sannidhiteredesai <sannidhiteredesai@users.noreply.github.com>
2019-04-15 22:46:54 +0530

    Fixed the test case for number_of_months()

--- pa/pa/report15g.py
+++ pa/pa/report15g.py
@@ -2,6 +2,7 @@ from pa.pa.fd import FDs
 import datetime
 from dateutil.relativedelta import relativedelta
 
+
 def get_nearest_start_of_month(date):
     first_day_of_this_month = date + relativedelta(day=1)
     first_day_of_next_month = date + relativedelta(day=1, months=+1)
@@ -9,8 +10,14 @@ def get_nearest_start_of_month(date):
     after_days = first_day_of_next_month - date
     return first_day_of_this_month if before_days <= after_days else first_day_of_next_month
 
+
 def number_of_months(date1, date2):
-    return (date2.year - date1.year) * 12 + date2.month - date1.month + 1
+    if date1.year == date2.year:
+        months = date2.month - date1.month + 1
+    else:
+        months = (date2.year - date1.year) * 12 + date2.month - date1.month + 1
+    return months if months > 0 else 0
+
 
 # def get_interest_for_next_year(principal, roi, start_date, end_date):
 #     year_start = datetime.datetime.utcnow().strftime("%Y")+'0401'
@@ -21,12 +28,12 @@ def number_of_months(date1, date2):
 #
 def generate_15g_report(for_member, for_user):
     fds = FDs()
-    print(fds.get_all_fds(for_user='u'))
     fds_for_member = fds.get_fds_with_first_name(first_name=for_member, for_user=for_user)
-
-    print(str(fds_for_member))
     for fd in fds_for_member:
-        print(fd)
-#
-# if __name__ == '__main__':
-#     generate_15g_report(for_member='aa', for_user='u')
+        if fd['type'] == 'Cumulative':
+            year_start = datetime.datetime.strptime(datetime.datetime.utcnow().strftime("%Y") + '0401', "%Y%m%d")
+            print(number_of_months(datetime.datetime.strptime(fd['start_date'], "%Y%m%d"), year_start))
+
+
+if __name__ == '__main__':
+    generate_15g_report(for_member='aa', for_user='u')
--- test/pa/test_report15g.py
+++ test/pa/test_report15g.py
@@ -19,10 +19,12 @@ class TestReport15G(TestCase):
         self.assertEqual(date(2019, 4, 1), get_nearest_start_of_month(date(2019, 3, 31)))
 
     def test_number_of_months_between_two_dates(self):
+        self.assertEqual(0, number_of_months(date(2019, 4, 1), date(2018, 4, 1)))
         self.assertEqual(1, number_of_months(date(2019, 4, 1), date(2019, 4, 1)))
         self.assertEqual(3, number_of_months(date(2019, 4, 1), date(2019, 6, 1)))
         self.assertEqual(12, number_of_months(date(2019, 4, 1), date(2020, 3, 1)))
         self.assertEqual(13, number_of_months(date(2019, 4, 1), date(2020, 4, 1)))
+        self.assertEqual(24, number_of_months(date(2018, 1, 1), date(2019, 12, 1)))
 
     # def test_start_date_less_than_fy(self):
     #     self.assertEqual(4466.20, get_interest_for_next_year(principal=1000,
