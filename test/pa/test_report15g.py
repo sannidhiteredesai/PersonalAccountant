e84b02854acd17fc12effe45f848cfafa5603961
sannidhiteredesai <sannidhiteredesai@users.noreply.github.com>
2019-04-26 23:47:37 +0530

    Comparing the type of period in tests

--- test/pa/test_report15g.py
+++ test/pa/test_report15g.py
@@ -41,33 +41,34 @@ class TestInterest(TestCase):
 
 class TestPeriodBetween(TestCase):
 
-    # TODO - Use class instead of tuple / change the attribute period
+    def compare(self, expected_period, actual_period):
+        assertion_error = f'{expected_period} != {actual_period}'
+        if expected_period != actual_period: raise AssertionError(assertion_error)
+        for e, a in zip(expected_period, actual_period):
+            if type(e) != type(a): raise AssertionError(assertion_error)
+        return True
 
     def test_end_date_less_or_equal_to_start_date(self):
-        self.assertEqual([], get_period_between(date(2019, 12, 31), date(2018, 3, 1)))
-        self.assertEqual([], get_period_between(date(2019, 12, 31), date(2019, 12, 31)))
+        self.compare([], get_period_between(date(2019, 12, 31), date(2018, 3, 1)))
+        self.compare([], get_period_between(date(2019, 12, 31), date(2019, 12, 31)))
 
     def test_dates_in_same_month(self):
-        self.assertEqual([Days(9)], get_period_between(date(2019, 1, 1), date(2019, 1, 10)))
-        self.assertEqual([Days(1)], get_period_between(date(2019, 1, 12), date(2019, 1, 13)))
-        self.assertEqual([Days(6)], get_period_between(date(2019, 1, 25), date(2019, 1, 31)))
+        self.compare([Days(9)], get_period_between(date(2019, 1, 1), date(2019, 1, 10)))
+        self.compare([Days(1)], get_period_between(date(2019, 1, 12), date(2019, 1, 13)))
+        self.compare([Days(6)], get_period_between(date(2019, 1, 25), date(2019, 1, 31)))
 
     def test_dates_with_start_of_month_in_year(self):
-        self.assertEqual([Months(1)], get_period_between(date(2019, 1, 1), date(2019, 2, 1)))
-        self.assertEqual([Months(3)], get_period_between(date(2019, 1, 1), date(2019, 4, 1)))
+        self.compare([Months(1)], get_period_between(date(2019, 1, 1), date(2019, 2, 1)))
+        self.compare([Months(3)], get_period_between(date(2019, 1, 1), date(2019, 4, 1)))
 
     def test_dates_in_same_year(self):
-        self.assertEqual([Days(1)], get_period_between(date(2019, 1, 29), date(2019, 1, 30)))
-        self.assertEqual([Days(period=3),
-                          Months(period=1),
-                          Days(period=29)], get_period_between(date(2019, 1, 29), date(2019, 3, 30)))
+        self.compare([Days(1)], get_period_between(date(2019, 1, 29), date(2019, 1, 30)))
+        self.compare([Days(3), Months(1), Days(29)], get_period_between(date(2019, 1, 29), date(2019, 3, 30)))
 
     def test_dates_in_different_years(self):
-        self.assertEqual([Days(3)], get_period_between(date(2018, 12, 29), date(2019, 1, 1)))
-        self.assertEqual([Days(7)], get_period_between(date(2018, 12, 29), date(2019, 1, 5)))
-        self.assertEqual([Days(3),
-                          Months(period=1),
-                          Days(period=2)], get_period_between(date(2018, 12, 29), date(2019, 2, 3)))
+        self.compare([Days(3)], get_period_between(date(2018, 12, 29), date(2019, 1, 1)))
+        self.compare([Days(7)], get_period_between(date(2018, 12, 29), date(2019, 1, 5)))
+        self.compare([Days(3), Months(1), Days(2)], get_period_between(date(2018, 12, 29), date(2019, 2, 3)))
 
 
 class TestFinancialYear(TestCase):
