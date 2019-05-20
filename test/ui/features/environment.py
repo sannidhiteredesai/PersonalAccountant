4c8d91801c84e9a311ad0767224f83700284e455
sannidhiteredesai <sannidhiteredesai@users.noreply.github.com>
2019-05-20 20:57:58 +0530

    Using before_all and after_all instead of before_feature and after_feature

--- test/ui/features/environment.py
+++ test/ui/features/environment.py
@@ -17,7 +17,7 @@ chrome_options.add_argument("--proxy-bypass-list=*")
 CHROME_DRIVER = os.path.join(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'driver'), 'chromedriver.exe')
 
 
-def before_feature(context, feature):
+def before_all(context):
     context.server = simple_server.WSGIServer(("", 5000), WSGIRequestHandler)
     context.server.set_app(app)
     context.pa_app = threading.Thread(target=context.server.serve_forever)
@@ -27,7 +27,7 @@ def before_feature(context, feature):
     context.browser.set_page_load_timeout(time_to_wait=200)
 
 
-def after_feature(context, feature):
+def after_all(context):
     context.browser.quit()
     context.server.shutdown()
     context.pa_app.join()
