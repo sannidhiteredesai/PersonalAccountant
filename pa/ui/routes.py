c3477715b07bdca78d1b5b025859c78aade54054
sannidhiteredesai <sannidhiteredesai@users.noreply.github.com>
2019-05-21 23:35:47 +0530

    Changing all references from index to home and adding separate index.html

--- pa/ui/routes.py
+++ pa/ui/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from flask_login import login_required, current_user
 from pa.ui import app
 from pa.ui.auth import routes
@@ -8,6 +8,14 @@ from pa.ui.bank import routes
 from pa.ui.fd import routes
 from pa.ui.report15g import routes
 from pa.ui.notifications import routes
+
+
+@app.route('/')
+def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    return render_template('index/index.html')
+
 
 @app.route('/home')
 @login_required
