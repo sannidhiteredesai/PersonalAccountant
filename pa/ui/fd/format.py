55a995c34f4f182951d770ea8011abcd31fbc216
sannidhiteredesai <sannidhiteredesai@users.noreply.github.com>
2019-05-24 00:07:09 +0530

    Adding logic to display all fds of one bank in same background colour

--- pa/ui/fd/format.py
+++ pa/ui/fd/format.py
@@ -6,6 +6,7 @@
 def append_serial_number(all_fds):
     """
     This function will append bank wise serial number starting from 1 to every fd
+    and also add the block value even/odd to it such that all fds in a bank have same block value
     :param all_fds - <list<dict>>
     :return:  None - as all_fds are modified in place
     """
@@ -15,10 +16,16 @@ def append_serial_number(all_fds):
 
     current_bank = all_fds[0]['bank_name'] if all_fds else ''
     serial_number = 0
+    block = 'even'
 
     # Append the serial number to fd dict, where the serial number starts from 1 for every bank
     for f in all_fds:
-        if current_bank == f['bank_name']: serial_number += 1
-        else: serial_number = 1
+        if current_bank == f['bank_name']:
+            serial_number += 1
+        else:
+            serial_number = 1
+            block = 'odd' if block == 'even' else 'even'
+
         current_bank = f['bank_name']
         f['serial_number'] = serial_number
+        f['block'] = block
